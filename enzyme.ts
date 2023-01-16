d16d220d5f5bc0f42d797fec106c2b623382d1a1
Daniel Trojer <daniel.trojer@unwired.at>
2023-01-16 18:12:14 +0100

    chore(deps): support react 18

--- enzyme.ts
+++ enzyme.ts
@@ -1,3 +1,4 @@
-import { configure } from 'enzyme'
-import * as Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-configure({ adapter: new Adapter() })
+import { configure } from "enzyme";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
+
+configure({ adapter: new Adapter() });
